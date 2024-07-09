fa95c6a3da0035374a7f4e7224fea7dcbc04c28e
Leandro Pereira <leandrog.pereira@sicoob.com.br>
2024-07-09 11:47:47 -0300

    First commit

---
+++ lib/resposta.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  final String texto;
+  final void Function() quandoSelecionado;
+  Resposta(this.texto, this.quandoSelecionado);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            child: Text(texto, style: TextStyle(color: Colors.white)),
+            onPressed: quandoSelecionado));
+  }
+}
